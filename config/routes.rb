6e25935f5ab9b7a0c23219ece8846ce70c530fd2
anhnd <ducanhst962@gmail.com>
2018-06-01 08:09:49 +0700

    Chapter 5: Filling in the layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_page/home'
-  get 'static_page/help'
-  get 'static_page/about'
-  root 'static_page#home'
+  get "/signup", to: "users#new"
+  get "/contact", to: "static_page#contact"
+  get "/help", to: "static_page#help"
+  get "/about", to: "static_page#about"
+  root "static_page#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
