5924c29af71abf19729db4eaa1d7738c313a48c0
anhnd <ducanhst962@gmail.com>
2018-06-13 15:02:47 +0700

    Chapter 10: Updating, showing, and deleting users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,16 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :admin_user, only: :destroy
+  before_action :find_user, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:edit, :update]
+  def index
+    @users = User.paginate page: params[:page]
   end
 
-  def show
-    @user = User.find_by id: params[:id]
-    return if @user
-    flash[:danger] = t ".flash_for_danger"
-    redirect_to root_path
+  def show; end
+
+  def new
+    @user = User.new
   end
 
   def create
@@ -21,10 +24,49 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit; end
+
+  def update
+    if @user.update_attributes user_params
+      flash[:success] = t ".flash_success"
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user.destroy
+    flash[:success] = t ".flash_success"
+    redirect_to users_url
+  end
+
   private
+
+  def logged_in_user
+    return if logged_in?
+    store_location
+    flash[:danger] = t "users.logged_in_user.flash_create_danger"
+    redirect_to login_url
+  end
+
+  def correct_user
+    redirect_to root_url unless current_user? @user
+  end
+
+  def admin_user
+    redirect_to root_url unless current_user.admin?
+  end
 
   def user_params
     params.require(:user).permit :name, :email, :password,
       :password_confirmation
+  end
+
+  def find_user
+    @user = User.find_by id: params[:id]
+    return if @user
+    flash[:danger] = t ".flash_for_danger"
+    redirect_to root_path
   end
 end
